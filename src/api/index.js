618cf013f4aed69cd124a9492e4156380731d3d2
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2023-03-09 16:21:03 -0300

    redux-hook with thunk

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,11 @@ export const getPokemon = () => {
   return axios
     .get('https://pokeapi.co/api/v2/pokemon?limit=151')
     .then((res) => res.data.results)
-    .catch((err) => console.log(err));
+    .catch((err) => console.log("error getpokemon",err));
 };
 
 export const getPokemonDetails = (pokemon) => {
   return axios.get(pokemon.url)
   .then(res => res.data)
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("error pokemon details",err));
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import {
   applyMiddleware,
   compose,
   legacy_createStore as createStore,
 } from "redux";
 import { pokemonsReducer } from "./reducers/pokemons";
-import {  logger } from "./middlewares";
+import { logger } from "./middlewares";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const composedEnhacers = compose(
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger )
-);
-const store = createStore(pokemonsReducer, composedEnhacers);
+
+const composeAlt = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const composedEnhancers = composeAlt(applyMiddleware(thunk, logger));
+
+const store = createStore(pokemonsReducer, composedEnhancers);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
